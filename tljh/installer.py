cf3d327f103b81d7571bb8b939424db8b7280d53
Yuvi Panda <yuvipanda@gmail.com>
2019-06-01 01:00:03 -0700

    Revert "Upgrade to JupyterHub 1.0"

--- tljh/installer.py
+++ tljh/installer.py
@@ -202,13 +202,13 @@ def ensure_jupyterhub_package(prefix):
     ])
 
     conda.ensure_pip_packages(prefix, [
-        'jupyterhub==1.0.0',
+        'jupyterhub==0.9.6',
         'jupyterhub-dummyauthenticator==0.3.1',
         'jupyterhub-systemdspawner==0.13',
         'jupyterhub-firstuseauthenticator==0.12',
         'jupyterhub-nativeauthenticator==0.0.4',
         'jupyterhub-ldapauthenticator==1.2.2',
-        'oauthenticator==0.8.2',
+        'oauthenticator==0.8.1'
     ])
     traefik.ensure_traefik_binary(prefix)
 
@@ -250,7 +250,7 @@ def ensure_user_environment(user_requirements_txt_file):
 
     conda.ensure_pip_packages(USER_ENV_PREFIX, [
         # JupyterHub + notebook package are base requirements for user environment
-        'jupyterhub==1.0.0',
+        'jupyterhub==0.9.6',
         'notebook==5.7.8',
         # Install additional notebook frontends!
         'jupyterlab==0.35.4',
@@ -262,7 +262,7 @@ def ensure_user_environment(user_requirements_txt_file):
         # Most people consider ipywidgets to be part of the core notebook experience
         'ipywidgets==7.4.2',
         # Pin tornado
-        'tornado<6.0',
+        'tornado<6.0'
     ])
 
     if user_requirements_txt_file:
